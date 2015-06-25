bb2df4b235de39c2905b7145f4e2174fadd458bf
Imad <i.ajallal@gmail.com>
2015-06-25 19:15:54 +0000

    Working

--- MatchWithCoreData/mainTableViewController.swift
+++ MatchWithCoreData/mainTableViewController.swift
@@ -47,7 +47,8 @@ class mainTableViewController: UITableViewController {
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 1
+        if (self.matchList.count > 0 ) {return 1}
+        else {return 0}
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -83,31 +84,53 @@ class mainTableViewController: UITableViewController {
             
         }
     }
-    
-    override func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
-        self.performSegueWithIdentifier("editExistantSegue", sender: self)
-    }
 
 
-    /*
-    // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
 
-    /*
+
+    
     // Override to support editing the table view.
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+            do {
+            // Delete the Object from the database
+                let appDel : AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+                let context : NSManagedObjectContext = appDel.managedObjectContext
+                context.deleteObject(self.matchList[indexPath.row] as! NSManagedObject)
+                    do {
+                    
+                        try context.save()
+                     }
+                    catch{
+                        print(error)
+                    }
+                let requ = NSFetchRequest(entityName: "Match")
+                try matchList = context.executeFetchRequest(requ)
+            }
+            catch {
+                print(error)
+            }
+            
+            // Delete from tableView
+            if (self.matchList.count>0) {
+            
+                tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+                
+            }
+            
+                else {
+                
+                tableView.deleteSections(NSIndexSet(index: indexPath.row), withRowAnimation: UITableViewRowAnimation.Fade)
+            
+            }
+        
         } else if editingStyle == .Insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
 
     /*
     // Override to support rearranging the table view.
